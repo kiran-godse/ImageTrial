2b9e6442b565705ae7f6ef869cb84bb068da7d19
kiran-godse <82302395+kiran-godse@users.noreply.github.com>
2023-07-11 17:32:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
+const core = require('@actions/core');
 const { execSync } = require('child_process');
 
-function runDockerContainer(imageName) {
-  try {
-    const command = `docker run ${imageName}`;
-    const output = execSync(command).toString();
-    console.log(output);
-  } catch (error) {
-    console.error('Error running Docker container:', error.message);
-  }
+try {
+  // Get the image name from the action input
+  const imageName = core.getInput('image-name');
+
+  // Execute the Docker inspect command to get the image metadata
+  const inspectCommand = `docker inspect ${imageName}`;
+  const output = execSync(inspectCommand).toString();
+  const imageMetadata = JSON.parse(output)[0];
+
+  // Extract the desired metadata properties
+  const metadata = {
+    id: imageMetadata.Id,
+    created: imageMetadata.Created,
+    size: imageMetadata.Size
+  };
+
+  // Set the metadata as action outputs
+  core.setOutput('metadata', JSON.stringify(metadata));
+} catch (error) {
+  core.setFailed(error.message);
 }
-
-// Get the image name from an input or pass it as an argument
-const imageName = process.argv[2] || 'nginx:latest';
-
-runDockerContainer(imageName);
