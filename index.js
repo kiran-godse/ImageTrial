f92a56b39d30da141cdf13d0d35cbccdf0a7086e
kiran-godse <82302395+kiran-godse@users.noreply.github.com>
2023-07-15 12:00:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,11 @@
 const core = require('@actions/core');
-const { execSync } = require('child_process');
 
 try {
-  // Get the image name from the action input
-  const imageName = core.getInput('image-name');
-
-  // Execute the Docker inspect command to get the image metadata
-  const inspectCommand = `docker inspect ${imageName}`;
-  const output = execSync(inspectCommand).toString();
-  const imageMetadata = JSON.parse(output)[0];
-
-  // Extract the desired metadata properties
-  const metadata = {
-    id: imageMetadata.Id,
-    created: imageMetadata.Created,
-    size: imageMetadata.Size
-  };
-
-  // Set the metadata as action outputs
-  core.setOutput('metadata', JSON.stringify(metadata));
+  const substrateJson = core.getInput('substrate_json');
+  const substrateObject = JSON.parse(substrateJson);
+  
+  const version = substrateObject.tag;
+  core.setOutput('version', version);
 } catch (error) {
-  core.setFailed(error.message);
+  core.setFailed(`Error occurred while getting the substrate version: ${error.message}`);
 }
